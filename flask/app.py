db8f30dd551bd1b382bc90256dc82d0313e57dd0
KIMATA Tetsuya <kimata@green-rabbit.net>
2018-12-30 09:41:22 +0900

    fix: カレントディレクトリの移動を削除
    app.py の実行のさせ方によってエラーになる為．
    Flask はスクリプト自身の更新を行っており，それとの相性が悪い．

--- flask/app.py
+++ flask/app.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import os
 from flask import Flask, render_template, request, redirect, url_for, jsonify, current_app, Response, send_from_directory
 
 from rasp_water import rasp_water
-
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 app = Flask(__name__)
 
