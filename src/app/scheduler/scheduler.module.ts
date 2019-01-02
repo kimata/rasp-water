3f429f6cb324b69cc47b876f19bb755a64c5abf1
KIMATA Tetsuya <kimata@green-rabbit.net>
2019-01-02 17:59:38 +0900

    feat: ng-tempusdominus-bootstrap4 の使用を廃止

--- src/app/scheduler/scheduler.module.ts
+++ src/app/scheduler/scheduler.module.ts
@@ -3,14 +3,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms'
 
-import { NgTempusdominusBootstrapModule } from 'ng-tempusdominus-bootstrap4';
 import { ToastrModule } from 'ngx-toastr';
 
 import { SchedulerEntryComponent } from './scheduler-entry/scheduler-entry.component';
 import { SchedulerControlComponent } from './scheduler-control/scheduler-control.component';
 
 @NgModule({
-    imports:      [ CommonModule, FormsModule, NgTempusdominusBootstrapModule, BrowserAnimationsModule, ToastrModule.forRoot() ],
+    imports:      [ CommonModule, FormsModule, BrowserAnimationsModule, ToastrModule.forRoot() ],
     declarations: [ SchedulerEntryComponent, SchedulerControlComponent ],
     exports:      [ SchedulerEntryComponent, SchedulerControlComponent ]
 })
