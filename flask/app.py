7b67861de2271bc70df3fa18bd918c99e4eaad9d
KIMATA Tetsuya <kimata@green-rabbit.net>
2019-03-20 22:21:26 +0900

    fix: メール送信を行う場所を変更

--- flask/app.py
+++ flask/app.py
@@ -9,16 +9,15 @@ from flask import Flask
 
 from rasp_water import rasp_water
 
-subprocess.call(
-    'echo "{} に再起動しました．" | mail -s "rasp-water 再起動" root'.format(
-        datetime.datetime.today()
-    ), shell=True
-)
-
 app = Flask(__name__)
 
 app.register_blueprint(rasp_water)
 
 if __name__ == '__main__':
+    subprocess.call(
+        'echo "{} に再起動しました．" | mail -s "rasp-water 再起動" root'.format(
+            datetime.datetime.today()
+        ), shell=True
+    )
     app.debug = True
     app.run(host='0.0.0.0', threaded=True)
