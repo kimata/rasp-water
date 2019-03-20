0cbdcd34bac48b075ffa7e36bebc81f1c536a9ab
KIMATA Tetsuya <kimata@green-rabbit.net>
2019-03-20 22:19:50 +0900

    feat: 再起動時にメールで通知するようにした

--- flask/app.py
+++ flask/app.py
@@ -2,9 +2,18 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import datetime
+import subprocess
+
 from flask import Flask
 
 from rasp_water import rasp_water
+
+subprocess.call(
+    'echo "{} に再起動しました．" | mail -s "rasp-water 再起動" root'.format(
+        datetime.datetime.today()
+    ), shell=True
+)
 
 app = Flask(__name__)
 
