3e8e42585025a443ef2f5fdefe26b9b8cd9e18c3
KIMATA Tetsuya <kimata@green-rabbit.net>
2019-04-18 01:28:25 +0900

    fix: influxdb が None を返すことを考慮し忘れていた問題を修正

--- flask/soilwet_check.py
+++ flask/soilwet_check.py
@@ -8,19 +8,26 @@ INFLUXDB_PORT = 8086
 INFLUXDB_DB = 'sensor'
 
 INFLUXDB_QUERY = """
-SELECT mean("touchpad") FROM "sensor.esp32" WHERE ("hostname" = \'ESP32-raindrop\') AND time >= now() - 1h GROUP BY time(5m) fill(previous) ORDER by time desc LIMIT 1
+SELECT mean("touchpad") FROM "sensor.esp32" WHERE ("hostname" = \'ESP32-raindrop\') AND time >= now() - 1h GROUP BY time(5m) fill(previous) ORDER by time desc LIMIT 10
 """
 
 WET_THRESHOLD = 380
+
+def get_latest_mean(result):
+    for status in result.get_points():
+        if not status['mean'] is None:
+            return status['mean']
+    return None
 
 def is_soil_wet():
     try:
         client = InfluxDBClient(host=INFLUXDB_ADDR, port=INFLUXDB_PORT, database=INFLUXDB_DB)
         result = client.query(INFLUXDB_QUERY)
-        status = result.get_points().__next__()['mean']
-        if status is None:
+        mean = get_latest_mean(result)
+
+        if mean is None:
             return False
-        return status < WET_THRESHOLD
+        return mean < WET_THRESHOLD
     except:
         pass
 
