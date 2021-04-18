ca4f31403bab455683b23206846533317e793d27
KIMATA Tetsuya <kimata@green-rabbit.net>
2021-04-18 16:24:53 +0900

    fix: ダミーリードを追加

--- script/sync_overlay.zsh
+++ script/sync_overlay.zsh
@@ -1,8 +1,21 @@
 #!/usr/bin/zsh
 
+FILE_LIST=(
+    /var/spool/cron/crontabs/root
+    /var/log/rasp-water.db
+)
+
 if [ -e /media/root-ro ]; then
+    # Dummy read
+    for file in $FILE_LIST; do
+        cat $file
+    done
+
     sudo mount -o remount,rw /media/root-ro
-    sudo cp -f /var/spool/cron/crontabs/root /media/root-ro/var/spool/cron/crontabs/root
-    sudo cp -f /var/log/rasp-water.db /media/root-ro/var/log/rasp-water.db
+
+    for file in $FILE_LIST; do
+        sudo cp -f $file /media/root-ro$file
+    done
+
     sudo mount -o remount,ro /media/root-ro
 fi
