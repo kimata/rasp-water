3790633a2e64014021bfa6398fdb22edb89608ce
KIMATA Tetsuya <kimata@green-rabbit.net>
2021-04-18 15:58:19 +0900

    fix: ログも永続化するようにした

--- script/sync_overlay.zsh
+++ script/sync_overlay.zsh
@@ -3,5 +3,6 @@
 if [ -e /media/root-ro ]; then
     sudo mount -o remount,rw /media/root-ro
     sudo cp -f /var/spool/cron/crontabs/root /media/root-ro/var/spool/cron/crontabs/root
+    sudo cp -f /var/log/rasp-water.db /media/root-ro/var/log/rasp-water.db
     sudo mount -o remount,ro /media/root-ro
 fi
