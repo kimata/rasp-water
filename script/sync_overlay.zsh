462a83331d61d2657183cb84333cb31c1bd70426
KIMATA Tetsuya <kimata@green-rabbit.net>
2021-04-18 15:50:37 +0900

    fix: overlayroot を使っていない場合にエラーが出ないようにした

--- script/sync_overlay.zsh
+++ script/sync_overlay.zsh
@@ -1,5 +1,7 @@
 #!/usr/bin/zsh
 
-sudo mount -o remount,rw /media/root-ro
-sudo cp -f /var/spool/cron/crontabs/root /media/root-ro/var/spool/cron/crontabs/root
-sudo mount -o remount,ro /media/root-ro
+if [ -e /media/root-ro ]; then
+    sudo mount -o remount,rw /media/root-ro
+    sudo cp -f /var/spool/cron/crontabs/root /media/root-ro/var/spool/cron/crontabs/root
+    sudo mount -o remount,ro /media/root-ro
+fi
