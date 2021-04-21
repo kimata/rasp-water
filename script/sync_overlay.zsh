37aaa0c6e5ff427eebcf9255489049ae09051ce6
KIMATA Tetsuya <kimata@green-rabbit.net>
2021-04-21 15:37:54 +0900

    fix: path は別の意味になるので変数名を変更

--- script/sync_overlay.zsh
+++ script/sync_overlay.zsh
@@ -1,9 +1,9 @@
 #!/usr/bin/zsh
 
-path=$1
+file_path=$1
 
 if [ -e /media/root-ro ]; then
     sudo mount -o remount,rw /media/root-ro
-    sudo cp -f $path /media/root-ro$path
+    sudo cp -f $file_path /media/root-ro$file_path
     sudo mount -o remount,ro /media/root-ro
 fi
