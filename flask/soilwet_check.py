c3e07e522206489a875e0a01aa49f24d3f8b56ab
KIMATA Tetsuya <kimata@green-rabbit.net>
2021-04-26 16:27:17 +0900

    fix: 雨量計の計測値を考慮するようにした

--- flask/soilwet_check.py
+++ flask/soilwet_check.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import pprint
 from influxdb import InfluxDBClient
 from datetime import datetime
 
@@ -9,32 +10,64 @@ INFLUXDB_ADDR = '192.168.2.20'
 INFLUXDB_PORT = 8086
 INFLUXDB_DB = 'sensor'
 
-INFLUXDB_QUERY = """
+INFLUXDB_QUERY1 = """
 SELECT mean("touchpad") FROM "sensor.esp32" WHERE ("hostname" = \'ESP32-raindrop\') AND time >= now() - 1h GROUP BY time(5m) fill(previous) ORDER by time desc LIMIT 10
 """
 
-WET_THRESHOLD = 370
+INFLUXDB_QUERY2 = """
+SELECT sum("rain") FROM "sensor.esp32" WHERE ("hostname" = \'ESP32-rain\') AND time >= now() - 2d GROUP BY time(12h) fill(0) ORDER by time desc LIMIT 10
+"""
 
-def is_soil_wet():
+WET_THRESHOLD1 = 370
+WET_THRESHOLD2 = 0.5
+
+def is_soil_wet_1():
     try:
         client = InfluxDBClient(host=INFLUXDB_ADDR, port=INFLUXDB_PORT, database=INFLUXDB_DB)
-        result = client.query(INFLUXDB_QUERY)
+        result = client.query(INFLUXDB_QUERY1)
 
         points = list(filter(lambda x: not x is None,
                              map(lambda x: x['mean'], result.get_points())))
 
         with open(os.path.join(os.path.dirname(os.path.abspath(__file__)),
                                'soilwet.log'), mode='a') as f:
-            print('{} {}'.format(datetime.now(), list(points)), file=f)
+            print('{} wet1 {}'.format(datetime.now(), list(points)), file=f)
 
         val = points[0]
         if val is None:
             return False
-        return val < WET_THRESHOLD
+        return val < WET_THRESHOLD1
     except:
         pass
 
     return False
+
+
+def is_soil_wet_2():
+    try:
+        client = InfluxDBClient(host=INFLUXDB_ADDR, port=INFLUXDB_PORT, database=INFLUXDB_DB)
+        result = client.query(INFLUXDB_QUERY2)
+
+
+        points = list(filter(lambda x: not x is None,
+                             map(lambda x: x['sum'], result.get_points())))
+
+        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                               'soilwet.log'), mode='a') as f:
+            print('{} wet2 {}'.format(datetime.now(), list(points)), file=f)
+
+        val = points[0]
+        if val is None:
+            return False
+        return val > WET_THRESHOLD2
+    except:
+        pass
+
+    return False
+
+
+def is_soil_wet():
+    return is_soil_wet_1() or is_soil_wet_2()
 
 if __name__ == '__main__':
     print(is_soil_wet())
