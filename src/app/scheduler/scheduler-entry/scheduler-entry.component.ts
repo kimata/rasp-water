34f3114f412f27d5ec26e735d560577a534594dc
KIMATA Tetsuya <kimata@green-rabbit.net>
2023-06-11 21:38:39 +0900

    fix: コードを整理

--- src/app/scheduler/scheduler-entry/scheduler-entry.component.ts
+++ src/app/scheduler/scheduler-entry/scheduler-entry.component.ts
@@ -1,10 +1,16 @@
-import { Component, OnInit, EventEmitter, Input, Output, ChangeDetectorRef } from '@angular/core';
-import { SchedulerControlComponent } from '../scheduler-control/scheduler-control.component';
+import {
+    Component,
+    OnInit,
+    EventEmitter,
+    Input,
+    Output,
+    ChangeDetectorRef,
+} from "@angular/core";
+import { SchedulerControlComponent } from "../scheduler-control/scheduler-control.component";
 
-import * as uuid from 'uuid';
-import { NgFor } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
+import * as uuid from "uuid";
+import { NgFor } from "@angular/common";
+import { FormsModule } from "@angular/forms";
 
 export interface ScheduleEntry {
     time: string;
@@ -13,19 +19,18 @@ export interface ScheduleEntry {
     is_active: boolean;
 }
 @Component({
-    selector: 'app-scheduler-entry',
-    templateUrl: './scheduler-entry.component.html',
-    styleUrls: ['./scheduler-entry.component.scss'],
+    selector: "app-scheduler-entry",
+    templateUrl: "./scheduler-entry.component.html",
+    styleUrls: ["./scheduler-entry.component.scss"],
     standalone: true,
-    imports: [FormsModule, NgFor]
+    imports: [FormsModule, NgFor],
 })
-
 export class SchedulerEntryComponent implements OnInit {
     private _state = {
         time: "00:00",
         period: 0,
         wday: new Array<boolean>(7).fill(false),
-        is_active: false
+        is_active: false,
     };
 
     @Output() stateChange = new EventEmitter();
@@ -43,32 +48,34 @@ export class SchedulerEntryComponent implements OnInit {
     constructor(
         private control: SchedulerControlComponent,
         private changeDetectorRef: ChangeDetectorRef
-    ) { }
+    ) {}
 
-    id = uuid.v4();
+    readonly id = uuid.v4();
 
     timeOptions = {
-        format: 'HH:mm',
+        format: "HH:mm",
     };
 
-    ngOnInit() {
-    }
+    ngOnInit() {}
 
     update() {
-        if (this._state['wday'].filter((x: boolean) => x).length == 0) {
-            this._state['wday'] = new Array<boolean>(7).fill(true);
+        if (this._state["wday"].filter((x: boolean) => x).length == 0) {
+            this._state["wday"] = new Array<boolean>(7).fill(true);
         }
         this.stateChange.emit(this.state);
         this.control.onChange();
     }
 
-    changeWday(event: Event, i:number) {
-        const wday = [...this.state['wday']];
+    changeWday(event: Event, i: number) {
+        const wday = [...this.state["wday"]];
         wday[i] = !wday[i];
         if (wday.filter((x: Boolean) => x).length == 0) {
-            this.control.toast.show_info('いずれかの曜日を選択する必要があります。', {
-                title: '通知',
-            });
+            this.control.toast.show_info(
+                "いずれかの曜日を選択する必要があります。",
+                {
+                    title: "通知",
+                }
+            );
 
             event.preventDefault();
         }
