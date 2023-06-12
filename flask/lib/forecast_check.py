e9d98d2337ca1a8bdc6b533350b287df1ece0e81
KIMATA Tetsuya <kimata@green-rabbit.net>
2023-06-12 23:31:10 +0900

    fix: OpenWeatherMap 関係の関数を削除

--- flask/lib/forecast_check.py
+++ flask/lib/forecast_check.py
@@ -12,7 +12,6 @@ from dateutil import parser
 from config import *
 
 YAHOO_API_ENDPOINT = "https://map.yahooapis.jp/weather/V1/place"
-OPENWEATHERMAP_API_ENDPOINT = "http://api.openweathermap.org/data/2.5/forecast"
 
 
 def get_weather_info_yahoo():
@@ -43,53 +42,6 @@ def check_rain_fall_yahoo():
         ),
     )
     return functools.reduce(lambda x, y: x + y, rainfall_list)
-
-
-def get_weather_info_openweathermap():
-    params = {
-        "APPID": OPENWEATHERMAP_APP_ID,
-        "id": OPENWEATHERMAP_CITY_ID,
-    }
-    url = "{}?{}".format(OPENWEATHERMAP_API_ENDPOINT, urllib.parse.urlencode(params))
-
-    return json.loads(urllib.request.urlopen(url).read().decode("utf-8"))
-
-
-def check_rain_fall_openweathermap():
-    json = get_weather_info_openweathermap()
-    # OpenWeatherMap の場合，かなり先まで取得できるので，8 時間後までを参考にする
-    rainfall_list = list(
-        map(
-            lambda x: [
-                timezone("Asia/Tokyo")
-                .localize(datetime.fromtimestamp(x["dt"]))
-                .strftime("%c %z"),
-                x["rain"]["3h"],
-            ],
-            filter(
-                lambda x: (
-                    (datetime.fromtimestamp(x["dt"]) - datetime.now()).total_seconds()
-                    / (60 * 60)
-                )
-                < 8,
-                filter(lambda x: "rain" in x, json["list"]),
-            ),
-        )
-    )
-    with open(
-        os.path.join(os.path.dirname(os.path.abspath(__file__)), "forecast.log"),
-        mode="a",
-    ) as f:
-        print(
-            "{} {} => {}".format(
-                datetime.now(),
-                list(rainfall_list),
-                functools.reduce(lambda x, y: x + y[1], rainfall_list, 0.0),
-            ),
-            file=f,
-        )
-
-    return functools.reduce(lambda x, y: x + y[1], rainfall_list, 0.0)
 
 
 def is_rain_forecast():
