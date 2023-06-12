37578737774e4e77a9d8e2a780256b5d7165d005
KIMATA Tetsuya <kimata@green-rabbit.net>
2023-06-12 23:26:49 +0900

    fix: ファイルの場所を移動

---
+++ flask/lib/forecast_check.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import urllib.request
+import functools
+import os
+import json
+from pytz import timezone
+from datetime import datetime
+from dateutil import parser
+
+from config import *
+
+YAHOO_API_ENDPOINT = "https://map.yahooapis.jp/weather/V1/place"
+OPENWEATHERMAP_API_ENDPOINT = "http://api.openweathermap.org/data/2.5/forecast"
+
+
+def get_weather_info_yahoo():
+    params = {
+        "appid": YAHOO_APP_ID,
+        "coordinates": ",".join(map(str, [POINT_LON, POINT_LAT])),
+        "output": "json",
+        "past": 2,
+    }
+    url = "{}?{}".format(YAHOO_API_ENDPOINT, urllib.parse.urlencode(params))
+
+    return json.loads(urllib.request.urlopen(url).read().decode("utf-8"))
+
+
+def check_rain_fall_yahoo():
+    json = get_weather_info_yahoo()
+    weather_info = json["Feature"][0]["Property"]["WeatherList"]["Weather"]
+    # NOTE: YAhoo の場合，1 時間後までしか情報がとれないので，4 時間前以降を参考にする
+    rainfall_list = map(
+        lambda x: x["Rainfall"],
+        filter(
+            lambda x: (
+                datetime.now() - datetime.strptime(x["Date"], "%Y%m%d%H%M")
+            ).total_seconds()
+            / (60 * 60)
+            < 4,
+            weather_info,
+        ),
+    )
+    return functools.reduce(lambda x, y: x + y, rainfall_list)
+
+
+def get_weather_info_openweathermap():
+    params = {
+        "APPID": OPENWEATHERMAP_APP_ID,
+        "id": OPENWEATHERMAP_CITY_ID,
+    }
+    url = "{}?{}".format(OPENWEATHERMAP_API_ENDPOINT, urllib.parse.urlencode(params))
+
+    return json.loads(urllib.request.urlopen(url).read().decode("utf-8"))
+
+
+def check_rain_fall_openweathermap():
+    json = get_weather_info_openweathermap()
+    # OpenWeatherMap の場合，かなり先まで取得できるので，8 時間後までを参考にする
+    rainfall_list = list(
+        map(
+            lambda x: [
+                timezone("Asia/Tokyo")
+                .localize(datetime.fromtimestamp(x["dt"]))
+                .strftime("%c %z"),
+                x["rain"]["3h"],
+            ],
+            filter(
+                lambda x: (
+                    (datetime.fromtimestamp(x["dt"]) - datetime.now()).total_seconds()
+                    / (60 * 60)
+                )
+                < 8,
+                filter(lambda x: "rain" in x, json["list"]),
+            ),
+        )
+    )
+    with open(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "forecast.log"),
+        mode="a",
+    ) as f:
+        print(
+            "{} {} => {}".format(
+                datetime.now(),
+                list(rainfall_list),
+                functools.reduce(lambda x, y: x + y[1], rainfall_list, 0.0),
+            ),
+            file=f,
+        )
+
+    return functools.reduce(lambda x, y: x + y[1], rainfall_list, 0.0)
+
+
+def is_rain_forecast():
+    try:
+        # OpenWeatherMap は値がおかしいことが度々あったので，Yahoo を採用する
+        return check_rain_fall_yahoo() > 2
+    except:
+        pass
+
+    return False
+
+
+if __name__ == "__main__":
+    print(is_rain_forecast())
--- flask/lib/soilwet_check.py
+++ flask/lib/soilwet_check.py
@@ -6,9 +6,9 @@ import pprint
 from influxdb import InfluxDBClient
 from datetime import datetime
 
-INFLUXDB_ADDR = '192.168.0.10'
+INFLUXDB_ADDR = "192.168.0.10"
 INFLUXDB_PORT = 8086
-INFLUXDB_DB = 'sensor'
+INFLUXDB_DB = "sensor"
 
 INFLUXDB_QUERY1 = """
 SELECT mean("touchpad") FROM "sensor.esp32" WHERE ("hostname" = \'ESP32-raindrop\') AND time >= now() - 1h GROUP BY time(5m) fill(previous) ORDER by time desc LIMIT 10
@@ -21,17 +21,25 @@ SELECT sum("rain") FROM "sensor.esp32" WHERE ("hostname" = \'ESP32-rain\') AND t
 WET_THRESHOLD1 = 370
 WET_THRESHOLD2 = 0.5
 
+
 def is_soil_wet_1():
     try:
-        client = InfluxDBClient(host=INFLUXDB_ADDR, port=INFLUXDB_PORT, database=INFLUXDB_DB)
+        client = InfluxDBClient(
+            host=INFLUXDB_ADDR, port=INFLUXDB_PORT, database=INFLUXDB_DB
+        )
         result = client.query(INFLUXDB_QUERY1)
 
-        points = list(filter(lambda x: not x is None,
-                             map(lambda x: x['mean'], result.get_points())))
+        points = list(
+            filter(
+                lambda x: not x is None, map(lambda x: x["mean"], result.get_points())
+            )
+        )
 
-        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                               'soilwet.log'), mode='a') as f:
-            print('{} wet1 {}'.format(datetime.now(), list(points)), file=f)
+        with open(
+            os.path.join(os.path.dirname(os.path.abspath(__file__)), "soilwet.log"),
+            mode="a",
+        ) as f:
+            print("{} wet1 {}".format(datetime.now(), list(points)), file=f)
 
         val = points[0]
         if val is None:
@@ -45,16 +53,22 @@ def is_soil_wet_1():
 
 def is_soil_wet_2():
     try:
-        client = InfluxDBClient(host=INFLUXDB_ADDR, port=INFLUXDB_PORT, database=INFLUXDB_DB)
+        client = InfluxDBClient(
+            host=INFLUXDB_ADDR, port=INFLUXDB_PORT, database=INFLUXDB_DB
+        )
         result = client.query(INFLUXDB_QUERY2)
 
+        points = list(
+            filter(
+                lambda x: not x is None, map(lambda x: x["sum"], result.get_points())
+            )
+        )
 
-        points = list(filter(lambda x: not x is None,
-                             map(lambda x: x['sum'], result.get_points())))
-
-        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                               'soilwet.log'), mode='a') as f:
-            print('{} wet2 {}'.format(datetime.now(), list(points)), file=f)
+        with open(
+            os.path.join(os.path.dirname(os.path.abspath(__file__)), "soilwet.log"),
+            mode="a",
+        ) as f:
+            print("{} wet2 {}".format(datetime.now(), list(points)), file=f)
 
         val = points[0]
         if val is None:
@@ -69,5 +83,6 @@ def is_soil_wet_2():
 def is_soil_wet():
     return is_soil_wet_1() or is_soil_wet_2()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     print(is_soil_wet())
