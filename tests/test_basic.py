a7f96d83816f7d42bca1ca52098432773a22f429
KIMATA Tetsuya <kimata@green-rabbit.net>
2024-06-02 20:00:04 +0900

    fix: debug

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -279,14 +279,17 @@ def test_time(freezer):
     schedule.clear()
     job_time_str = time_str(time_test(1))
     logging.debug("set schedule at {time}".format(time=job_time_str))
-    job = schedule.every().day.at(job_time_str, TIMEZONE_PYTZ).do(lambda: True)
+
+    job_add = schedule.every().day.at(job_time_str, TIMEZONE_PYTZ).do(lambda: True)
+    schedule.every().day.at(time_str(time_test(10)), TIMEZONE_PYTZ).do(lambda: True)
+    schedule.every().day.at(time_str(time_test(20)), TIMEZONE_PYTZ).do(lambda: True)
 
     for i, job in enumerate(schedule.get_jobs()):
         logging.debug("Current schedule [{i}]: {next_run}".format(i=i, next_run=job.next_run))
 
     idle_sec = schedule.idle_seconds()
     logging.error("Time to next jobs is {idle:.1f} sec".format(idle=idle_sec))
-    logging.debug("Next run is {time}".format(time=job.next_run))
+    logging.debug("Next run is {time}".format(time=job_add.next_run))
 
     assert abs(idle_sec - 60) < 5
 
