08ec8e27271a36dcf4fa35fdabe52706d2a91bd5
KIMATA Tetsuya <kimata@green-rabbit.net>
2024-07-30 00:48:09 +0900

    fix: コードを整理

--- flask/lib/config.py
+++ flask/lib/config.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import logging
 import pathlib
 
 import yaml
@@ -7,5 +8,15 @@ CONFIG_PATH = "config.yaml"
 
 
 def load_config(config_path=CONFIG_PATH):
-    with pathlib.Path(config_path).resolve().open(mode="r") as file:
+    config_path = pathlib.Path(config_path).resolve()
+    logging.info("Load config: %s", config_path)
+    with config_path.open(mode="r") as file:
         return yaml.load(file, Loader=yaml.SafeLoader)
+
+
+if __name__ == "__main__":
+    import logger
+
+    logger.init("test", level=logging.INFO)
+
+    load_config()
