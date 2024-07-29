2a5d238a0836e3963a27a55202fd9f5fe428623e
KIMATA Tetsuya <kimata@green-rabbit.net>
2024-07-29 11:03:34 +0900

    fix: コードを整理

--- flask/lib/config.py
+++ flask/lib/config.py
@@ -6,28 +6,6 @@ import yaml
 CONFIG_PATH = "config.yaml"
 
 
-def abs_path(config_path=CONFIG_PATH):
-    return pathlib.Path(pathlib.Path.cwd(), config_path)
-
-
-# NOTE: プロジェクトによって，大文字と小文字が異なるのでここで吸収する
-def get_db_config(config):
-    if "INFLUXDB" in config:
-        return {
-            "token": config["INFLUXDB"]["TOKEN"],
-            "bucket": config["INFLUXDB"]["BUCKET"],
-            "url": config["INFLUXDB"]["URL"],
-            "org": config["INFLUXDB"]["ORG"],
-        }
-    else:
-        return {
-            "token": config["influxdb"]["token"],
-            "bucket": config["influxdb"]["bucket"],
-            "url": config["influxdb"]["url"],
-            "org": config["influxdb"]["org"],
-        }
-
-
 def load_config(config_path=CONFIG_PATH):
-    with abs_path(config_path).open(mode="r") as file:
+    with pathlib.Path(config_path).resolve().open(mode="r") as file:
         return yaml.load(file, Loader=yaml.SafeLoader)
