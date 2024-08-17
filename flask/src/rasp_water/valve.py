789d952e91faabfcd0f5410db48d1053e002cdc3
KIMATA Tetsuya <kimata@green-rabbit.net>
2024-08-17 23:09:10 +0900

    fix

--- flask/src/rasp_water/valve.py
+++ flask/src/rasp_water/valve.py
@@ -57,8 +57,8 @@ TIME_ZERO_TAIL = 5
 
 
 class VALVE_STATE(IntEnum):  # noqa: N801
-    OPEN = 1
-    CLOSE = 0
+    OPEN = my_lib.rpi.gpio.HIGH
+    CLOSE = my_lib.rpi.gpio.LOW
 
 
 class CONTROL_MODE(IntEnum):  # noqa: N801
